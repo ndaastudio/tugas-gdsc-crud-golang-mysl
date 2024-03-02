33dc10150118687d718395085d205e2901c7420e
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-03-03 02:29:25 +0700

    function untuk handle get user

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -19,3 +19,14 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 }
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	users, err := UserModel.GetUsers()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
