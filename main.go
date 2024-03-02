fd8fec4c1d12c34c812fc93718a3c298b9be2a3f
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-03-03 02:33:13 +0700

    load env dan koneksikan ke database

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	connection "crud-golang-mysql/configs"
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Terjadi kesalahan saat memuat file .env: %v", err)
+	}
+
+	connection.ConnectDB()
 }
