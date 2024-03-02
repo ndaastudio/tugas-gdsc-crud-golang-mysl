7668adb8868ce971ea0dc919363e3055f4f95dd4
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-03-03 02:34:14 +0700

    panggil routes dan koneksikan ke server

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	connection "crud-golang-mysql/configs"
+	"crud-golang-mysql/routes"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -14,4 +17,14 @@ func main() {
 	}
 
 	connection.ConnectDB()
+
+	router := routes.SetupRoutes()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server berjalan:", "http://localhost:"+port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
