dfe2c1688904a2327a69a1ea9c57a3bdee0e237c
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-03-03 02:24:35 +0700

    function untuk handle query insert

--- models/UserModel.go
+++ models/UserModel.go
@@ -1,7 +1,22 @@
 package UserModel
 
+import (
+	connection "crud-golang-mysql/configs"
+)
+
 type Schema struct {
 	ID   int    `json:"id"`
 	Nama string `json:"nama"`
 	Umur int    `json:"umur"`
+}
+
+func CreateUser(user *Schema) error {
+	db := connection.GetDB()
+
+	_, err := db.Exec("INSERT INTO users (nama, umur) VALUES (?, ?)", user.Nama, user.Umur)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
